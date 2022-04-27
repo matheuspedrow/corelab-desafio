395d33a2ad908e6a19b8dbfdd4aae25954fc2832
Matheus <matheuspedrow@gmail.com>
2022-04-27 10:38:26 -0300

    install styled components

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,22 @@
-import '../style.css'
+import { createGlobalStyle } from 'styled-components'
+
+const Globalstyle = createGlobalStyle `
+* {
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  font-family: 'Roboto', sans-serif;
+}
+`
 
 function App ({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <>
+      <Globalstyle />
+      <Component {...pageProps} />
+    </>
   )
 }
 
