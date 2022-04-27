2659ba1e79bf7112588e7cb32e4c4d1bd3006210
Matheus <matheuspedrow@gmail.com>
2022-04-27 11:58:25 -0300

    create components image

---
+++ pages/login.jsx
@@ -0,0 +1,11 @@
+import ImageWithSpace from "../src/components/layout/ImageWithSpace"
+
+function LoginPage () {
+  return (
+    <ImageWithSpace>
+      TESTE 123
+    </ImageWithSpace>
+  )
+}
+
+export default LoginPage
